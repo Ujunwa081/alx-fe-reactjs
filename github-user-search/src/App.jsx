e4f94c00e345fbe1f07646b8fbf37d87af85a0ee
ujunwa <atuujunwaperpetual@gmail.com>
2025-09-04 20:36:22 +0100

    Setup GitHub User Search React App with Vite and Axios

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search App</h1>
+      <p>Setup is complete. Ready for development 🚀</p>
+    </div>
+  );
+}
+
+export default App;
+
